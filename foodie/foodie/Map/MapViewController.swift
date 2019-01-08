cbd3852a4f0863057814d92da5ab8909225e76f4
Austin Du <akxdu@uwaterloo.ca>
2019-01-08 17:42:34 -0500

    put tab bar back in, removed upload and notifications tab, added map tab, added skeleton for MapViewController

---
+++ foodie/foodie/Map/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  foodie
+//
+//  Created by Austin Du on 2019-01-08.
+//  Copyright © 2019 JAY. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- foodie/foodie/TabBar/RootTabBarViewController.swift
+++ foodie/foodie/TabBar/RootTabBarViewController.swift
@@ -19,7 +19,6 @@ class RootTabBarViewController: UITabBarController {
             tabBarItem.title = nil
             tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
         }
-        tabBar.isHidden = true
     }
     
 }
